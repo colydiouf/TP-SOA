60a16104162c064e9b2fe4cbb84bd71ba6abff14
coly diouf <coly.diouf@uvs.edu.sn>
2019-12-27 13:47:54 +0100

    premier commit

---
+++ projetUVS/src/main/java/projetUVS/projetUVS/ProjetUvsApplication.java
@@ -0,0 +1,13 @@
+package projetUVS.projetUVS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetUvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetUvsApplication.class, args);
+	}
+
+}
